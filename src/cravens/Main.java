3ecfed16b5a3c5a2682e705dc3d2d95189a91f7f
Dixie Cravens <dcravens@gmail.com>
2021-10-12 16:53:35 -0400

    commit  for stretch activity

--- src/cravens/Main.java
+++ src/cravens/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Dixie.");
         System.out.println("Hi! It's Sara.");
+        System.out.println("I am going to delete this line.");
     }
 
 }
